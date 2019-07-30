7e331a4b34f26e3a8cec2a05324867c925e1d47b
user <user@DESKTOP-R83NG38>
2019-07-30 16:55:14 +0900

    hello

--- Ex03/Ex03.sql
+++ Ex03/Ex03.sql
@@ -12,9 +12,19 @@ create sequence seq_reply;
 alter table reply add constraint pk_reply primary key (rno);
 
 alter table reply add constraint fk_reply_board
-foreign key (bno) references board(bno);
+foreign key (bno) references tbl_board(bno);
 
 
 select * from board;
 
 select * from REPLY;
+
+create table board(
+bno number(10,0),
+title varchar2(200) not null,
+content varchar2(2000) not null,
+writer varchar2(50) not null,
+regdate date default sysdate,
+udatedate date default sysdate
+
+);
