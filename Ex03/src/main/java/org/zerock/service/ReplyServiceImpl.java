ad03d4b074b9a64426a233aa7cb7259b2c85164b
user <user@DESKTOP-R83NG38>
2019-08-01 17:20:42 +0900

    ex03/end

--- Ex03/src/main/java/org/zerock/service/ReplyServiceImpl.java
+++ Ex03/src/main/java/org/zerock/service/ReplyServiceImpl.java
@@ -2,56 +2,78 @@ package org.zerock.service;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.zerock.domain.Criteria;
+import org.zerock.domain.ReplyPageDTO;
 import org.zerock.domain.ReplyVO;
 import org.zerock.mapper.ReplyMapper;
 
-import lombok.Setter;
+import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
 
 @Service
 @Log4j
+@AllArgsConstructor
 public class ReplyServiceImpl implements ReplyService {
-	
-	@Setter
-	@Autowired
-	private ReplyMapper mapper;
-	
-	@Override
-	public int register(ReplyVO vo) {
-		log.info("register........" +vo);
-		return mapper.insert(vo);
-	}
 
-	@Override
-	public ReplyVO get(Long rno) {
-		// TODO Auto-generated method stub
-		log.info("get................"+rno);
-		return mapper.read(rno);
-	}
+  
+  private ReplyMapper mapper;
 
-	@Override
-	public int modify(ReplyVO vo) {
-		// TODO Auto-generated method stub
-		log.info("modify..........." + vo);
-		return mapper.update(vo);
-	}
+  
+  
+  @Override
+  public int register(ReplyVO vo) {
 
-	@Override
-	public int remove(Long rno) {
-		// TODO Auto-generated method stub
-		log.info("remove......."+rno);
-		
-		return mapper.delete(rno);
-	}
+    log.info("register......" + vo);
 
-	@Override
-	public List<ReplyVO> getList(Criteria cri, Long bno) {
-		// TODO Auto-generated method stub
-		log.info("get Reply List of a Board" + bno);
-		return mapper.getListWithPaging(cri, bno);
-	}
-	
+    return mapper.insert(vo);
+
+  }
+
+  @Override
+  public ReplyVO get(Long rno) {
+
+    log.info("get......" + rno);
+
+    return mapper.read(rno);
+
+  }
+
+  @Override
+  public int modify(ReplyVO vo) {
+
+    log.info("modify......" + vo);
+
+    return mapper.update(vo);
+
+  }
+
+  @Override
+  public int remove(Long rno) {
+
+    log.info("remove...." + rno);
+
+    return mapper.delete(rno);
+
+  }
+
+  @Override
+  public List<ReplyVO> getList(Criteria cri, Long bno) {
+
+    log.info("get Reply List of a Board " + bno);
+
+    return mapper.getListWithPaging(cri, bno);
+
+  }
+  
+  @Override
+  public ReplyPageDTO getListPage(Criteria cri, Long bno) {
+       
+    return new ReplyPageDTO(
+        mapper.getCountByBno(bno), 
+        mapper.getListWithPaging(cri, bno));
+  }
+
+
 }
+
